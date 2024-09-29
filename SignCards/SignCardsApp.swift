f8316cb23d35a45d43cc6628179d73693dd14717
Walaa <Walaa.matar20@gmail.com>
2024-09-29 16:22:07 +0300

    Initial Commit

---
+++ SignCards/SignCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignCardsApp.swift
+//  SignCards
+//
+//  Created by Walaa on 29/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SignCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SignCardsTests/SignCardsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SignCardsTests.swift
+//  SignCardsTests
+//
+//  Created by Walaa on 29/09/2024.
+//
+
+import Testing
+@testable import SignCards
+
+struct SignCardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
